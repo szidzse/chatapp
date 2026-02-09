7e0d656ab71c3da1f78b7c71ab70bbe6d7eec343
Sós Gellért <gellertsos@gmail.com>
2026-02-09 23:07:05 +0100

    chore: install helmet and cors, use middlewares

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -1,7 +1,23 @@
 import express, { type Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 export const createApp = (): Application => {
   const app = express();
+
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    }),
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use((_req, res) => {
+    res.status(404).json({ error: "Not Found" });
+  });
 
   return app;
 };
