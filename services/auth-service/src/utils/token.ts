258a101d6b40cd293e031ee0b453d9ee1245aad8
Sós Gellért <gellertsos@gmail.com>
2026-02-11 10:03:58 +0100

    feat: implement hashPassword fn

--- services/auth-service/src/utils/token.ts
+++ services/auth-service/src/utils/token.ts
@@ -1,2 +1,17 @@
+import { env } from "@/config/env";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { type Secret, type SignOptions } from "jsonwebtoken";
+
+const ACCESS_TOKEN: Secret = env.JWT_SECRET;
+const REFRESH_TOKEN: Secret = env.JWT_REFRESH_SECRET;
+const ACCESS_OPTIONS: SignOptions = {
+  expiresIn: env.JWT_EXPIRES_IN as SignOptions["expiresIn"],
+};
+const REFRESH_OPTIONS: SignOptions = {
+  expiresIn: env.JWT_REFRESH_EXPIRES_IN as SignOptions["expiresIn"],
+};
+
+export const hashPassword = async (password: string): Promise<string> => {
+  const saltRounds = 12;
+  return bcrypt.hash(password, saltRounds);
+};
