d3e260ac1746cd95a2141bacc3e2157f0e262154
Sós Gellért <gellertsos@gmail.com>
2026-02-11 10:13:06 +0100

    feat: implement createRefreshToken fn

--- services/auth-service/src/services/auth.service.ts
+++ services/auth-service/src/services/auth.service.ts
@@ -1,8 +1,12 @@
 import { sequelize } from "@/db/sequelize";
 import { UserCredentials } from "@/models";
 import { AuthResponse, RegisterInput } from "@/types/auth";
+import { hashPassword } from "@/utils/token";
 import { HttpError } from "@chatapp/common";
-import { Op } from "sequelize";
+import { Op, Transaction } from "sequelize";
+import crypto from "crypto";
+
+const REFRESH_TOKEN_TTL_DAYS = 30;
 
 export const register = async (input: RegisterInput): Promise<AuthResponse> => {
   const existing = await UserCredentials.findOne({
@@ -17,8 +21,35 @@ export const register = async (input: RegisterInput): Promise<AuthResponse> => {
 
   const transaction = await sequelize.transaction();
   try {
-    const passwordHash = await
-  } catch (error) {
+    const passwordHash = await hashPassword(input.password);
+    const user = await UserCredentials.create(
+      {
+        email: input.email,
+        displayName: input.displayName,
+        passwordHash,
+      },
+      { transaction },
+    );
+  } catch (error) {}
+};
 
-  }
+const createRefreshToken = async (
+  userId: string,
+  transaction?: Transaction,
+) => {
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + REFRESH_TOKEN_TTL_DAYS); // 30 days from now
+
+  const tokenId = crypto.randomUUID();
+
+  const record = await RefreshToken.create(
+    {
+      userId,
+      tokenId,
+      expiresAt,
+    },
+    { transaction },
+  );
+
+  return record;
 };
