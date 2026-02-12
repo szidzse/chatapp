674452c8cf0db5b730329471eeabb39fe5287f46
Sós Gellért <gellertsos@gmail.com>
2026-02-12 18:17:57 +0100

    chore: create logger, error handler middleware, env schema, and gateway server

--- services/gateway-service/src/config/env.ts
+++ services/gateway-service/src/config/env.ts
@@ -5,18 +5,14 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
-  AUTH_SERVICE_PORT: z.coerce.number().int().min(0).max(65_535).default(4003),
-  AUTH_DB_URL: z.string().url(),
-  JWT_SECRET: z.string().min(32),
-  JWT_EXPIRES_IN: z.string().default("1d"),
-  JWT_REFRESH_SECRET: z.string().min(32),
-  JWT_REFRESH_EXPIRES_IN: z.string().default("30d"),
+  GATEWAY_PORT: z.coerce.number().int().min(0).max(65_535).default(4000),
+  AUTH_SERVICE_URL: z.string().url(),
 });
 
 type EnvType = z.infer<typeof envSchema>;
 
 export const env: EnvType = createEnv(envSchema, {
-  serviceName: "auth-service",
+  serviceName: "gateway-service",
 });
 
 export type Env = typeof env;
