94d4ec12b5c34dffb12c18ebab75a2c36f0d1340
Sós Gellért <gellertsos@gmail.com>
2026-02-20 20:45:41 +0100

    chore: add INTERNAL_API_TOKEN env variable to .env, use it in envSchema

--- services/gateway-service/src/config/env.ts
+++ services/gateway-service/src/config/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
     .default("development"),
   GATEWAY_PORT: z.coerce.number().int().min(0).max(65_535).default(4000),
   AUTH_SERVICE_URL: z.string().url(),
+  INTERNAL_API_TOKEN: z.string().min(16),
 });
 
 type EnvType = z.infer<typeof envSchema>;
