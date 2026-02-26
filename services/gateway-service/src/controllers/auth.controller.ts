58979a8015c6ceb6ad2413a7036a2c2eef2be5f4
Sós Gellért <gellertsos@gmail.com>
2026-02-26 20:36:44 +0100

    feat: implement loginUser, refreshTokens and revokeTokens fns in auth controller

--- services/gateway-service/src/controllers/auth.controller.ts
+++ services/gateway-service/src/controllers/auth.controller.ts
@@ -1,5 +1,10 @@
 import { authProxyService } from "@/services/auth-proxy.service";
-import { registerSchema } from "@/validation/auth.schema";
+import {
+  loginSchema,
+  refreshSchema,
+  registerSchema,
+  revokeSchema,
+} from "@/validation/auth.schema";
 import { AsyncHandler } from "@chatapp/common";
 
 export const registerUser: AsyncHandler = async (req, res, next) => {
@@ -7,6 +12,36 @@ export const registerUser: AsyncHandler = async (req, res, next) => {
     const payload = registerSchema.parse(req.body);
     const response = await authProxyService.register(payload);
     res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser: AsyncHandler = async (req, res, next) => {
+  try {
+    const payload = loginSchema.parse(req.body);
+    const tokens = await authProxyService.login(payload);
+    res.json(tokens);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshTokens: AsyncHandler = async (req, res, next) => {
+  try {
+    const payload = refreshSchema.parse(req.body);
+    const tokens = await authProxyService.refresh(payload);
+    res.json(tokens);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const revokeTokens: AsyncHandler = async (req, res, next) => {
+  try {
+    const payload = revokeSchema.parse(req.body);
+    const tokens = await authProxyService.revoke(payload);
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
