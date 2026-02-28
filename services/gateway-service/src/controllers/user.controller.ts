0d24aaaed2443f8269c65fdccc2cb9ff5a612e7c
Sós Gellért <gellertsos@gmail.com>
2026-02-28 16:21:06 +0100

    feat: implement getUser fn in user controller

---
+++ services/gateway-service/src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { userProxyService } from "@/services/user-proxy.service";
+import { userIdParamsSchema } from "@/validation/user.schema";
+import { AsyncHandler } from "@chatapp/common";
+
+export const getUser: AsyncHandler = async (req, res, next) => {
+  try {
+    const { id } = userIdParamsSchema.parse(req.params);
+    const response = await userProxyService.getUserById(id);
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+};
