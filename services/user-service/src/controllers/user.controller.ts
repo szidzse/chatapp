b57ff77d84b154b07160aa226611e937471133e7
Sós Gellért <gellertsos@gmail.com>
2026-02-28 13:13:43 +0100

    feat: implement getUser fn in user controllers

--- services/user-service/src/controllers/user.controller.ts
+++ services/user-service/src/controllers/user.controller.ts
@@ -1,8 +1,13 @@
 import { userService } from "@/services/user.service";
+import { UserIdParams } from "@/validation/user.schema";
 import type { AsyncHandler } from "@chatapp/common";
 
-export const getUser: AsyncHandler = async (req, res) => {
+export const getUser: AsyncHandler = async (req, res, next) => {
   try {
-    const {} = req.params as unknown as { id: string };
-  } catch (error) {}
+    const { id } = req.params as unknown as UserIdParams;
+    const user = await userService.getUserById(id);
+    res.json({ data: user });
+  } catch (error) {
+    next(error);
+  }
 };
