2dab894fd1bd98fac5e2900938e66c5a0dac7283
Sós Gellért <gellertsos@gmail.com>
2026-02-28 16:27:27 +0100

    feat: implement searchUsers fn in user controller

--- services/gateway-service/src/controllers/user.controller.ts
+++ services/gateway-service/src/controllers/user.controller.ts
@@ -1,5 +1,11 @@
 import { userProxyService } from "@/services/user-proxy.service";
-import { createUserSchema, userIdParamsSchema } from "@/validation/user.schema";
+import { getAuthenticatedUser } from "@/utils/auth";
+import {
+  createUserSchema,
+  SearchUsersQuery,
+  searchUsersQuerySchema,
+  userIdParamsSchema,
+} from "@/validation/user.schema";
 import { AsyncHandler } from "@chatapp/common";
 
 export const getUser: AsyncHandler = async (req, res, next) => {
@@ -26,6 +32,30 @@ export const createUser: AsyncHandler = async (req, res, next) => {
     const payload = createUserSchema.parse(req.body);
     const response = await userProxyService.createUser(payload);
     res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const searchUsers: AsyncHandler = async (req, res, next) => {
+  try {
+    const user = getAuthenticatedUser(req);
+
+    const parsedQuery: SearchUsersQuery = searchUsersQuerySchema.parse(
+      req.query,
+    );
+
+    const { query, limit, exclude } = parsedQuery;
+
+    const sanitizedExclude = Array.from(new Set([...exclude, user.id]));
+
+    const users = await userProxyService.searchUsers({
+      query,
+      limit,
+      exclude: sanitizedExclude,
+    });
+
+    res.json({ data: users });
   } catch (error) {
     next(error);
   }
