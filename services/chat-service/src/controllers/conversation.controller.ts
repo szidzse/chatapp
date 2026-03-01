e75cdc87babf1cb9d43702c6aaef197982e616a9
Sós Gellért <gellertsos@gmail.com>
2026-03-01 21:21:23 +0100

    feat: implement createConversation fn for conversation controller

--- services/chat-service/src/controllers/conversation.controller.ts
+++ services/chat-service/src/controllers/conversation.controller.ts
@@ -5,3 +5,34 @@ import {
   createConversationSchema,
   listConversationQuerySchema,
 } from "@/validation/conversation.schema";
+import { conversationIdParamsSchema } from "@/validation/shared.schema";
+import { getAuthenticatedUser } from "@/utils/auth";
+
+const parsedConversation = (params: unknown) => {
+  const { id } = conversationIdParamsSchema.parse(params);
+  return id;
+};
+
+export const createConversation: RequestHandler = asyncHandler(
+  async (req, res) => {
+    const user = getAuthenticatedUser(req);
+    const payload = createConversationSchema.parse(req.body);
+    const uniqueParticipantIds = Array.from(
+      new Set([...payload.participantIds, user.id]),
+    );
+
+    if (uniqueParticipantIds.length < 2) {
+      throw new HttpError(
+        400,
+        "Conversation must atleast include one other participant",
+      );
+    }
+
+    const conversation = await conversationService.createConversation({
+      title: payload.title,
+      participantIds: uniqueParticipantIds,
+    });
+
+    res.status(201).json({ data: conversation });
+  },
+);
